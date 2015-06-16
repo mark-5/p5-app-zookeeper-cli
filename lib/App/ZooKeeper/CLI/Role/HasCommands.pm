c5f644d531319186c0f49d4530f31779fceb3089
mark-5 <maflick88@gmail.com>
2015-06-15 23:09:54 -0400

    refactor HasCommands role

--- lib/App/ZooKeeper/CLI/Role/HasCommands.pm
+++ lib/App/ZooKeeper/CLI/Role/HasCommands.pm
@@ -10,140 +10,197 @@ has commands => (
     default => sub { {} },
 );
 
-sub add_command {
-    my ($self, $name, @args) = @_;
-    $self->commands->{$name} = App::ZooKeeper::CLI::Command->new(
-        name => $name,
+our %_COMMANDS;
+sub add_cmd {
+    my ($name, @args) = @_;
+    $_COMMANDS{$name} = {
+        name   => $name,
+        method => "cmd_$name",
         @args,
-    );
+    };
 }
 
-sub BUILD {}
-after BUILD => sub {
+sub _setup_cmds {
     my ($self) = @_;
     weaken($self);
 
-    $self->add_command("add_auth", code => sub {
-        my ($opts, $args) = @_;
-        my ($scheme, $creds) = @$args;
-        $self->handle->add_auth($scheme, $creds, %$opts);
-        return;
-    }, opt_spec => [
+    while (my ($cmd, $args) = each %_COMMANDS) {
+        my $method = $self->can($args->{method});
+        $self->commands->{$cmd} = App::ZooKeeper::CLI::Command->new(
+            code => sub { $self->$method(@_) },
+            %$args,
+        );
+    }
+}
+
+add_cmd add_auth => (
+    opt_spec => [
         [ "encoded|e" ]
-    ], usage_desc => "<scheme> <credentials>");
+    ],
+    usage_desc => "<scheme> <credentials>",
+);
+sub cmd_add_auth {
+    my ($self, $opts, $args) = @_;
+    my ($scheme, $creds) = @$args;
+    $self->handle->add_auth($scheme, $creds, %$opts);
+    return;
+}
 
-    $self->add_command("cd", code => sub {
-        my ($opts, $args) = @_;
-        my $path = $args->[0] // "/";
-        $path = $self->previous_node if $path eq '-';
-        $path = qualify_path($path => $self->current_node);
-        die "Node $path does not exist\n" unless $self->handle->exists($path);
+add_cmd cd => (
+    usage_desc => "<path>",
+);
+sub cmd_cd {
+    my ($self, $opts, $args) = @_;
+    my $path = $args->[0] // "/";
+    $path = $self->previous_node if $path eq '-';
+    $path = qualify_path($path => $self->current_node);
+    die "Node $path does not exist\n" unless $self->handle->exists($path);
 
-        $self->previous_node($self->current_node);
-        $self->current_node($path);
-        return;
-    }, usage_desc => "<path>");
+    $self->previous_node($self->current_node);
+    $self->current_node($path);
+    return;
+}
 
-    $self->add_command("create", code => sub {
-        my ($opts, $args) = @_;
-        my $path = qualify_path($args->[0] => $self->current_node);
-        $opts->{value} = defined $args->[1] ? $args->[1] : "";
-        $self->handle->create($path, persistent => 1, %$opts);
-        return;
-    }, opt_spec => [
+add_cmd create => (
+    opt_spec => [
         [ "persistent|p" ],
-        [ "sequential|s" ]
-    ], usage_desc => "<path> <value>");
+        [ "sequential|s" ],
+    ],
+    usage_desc => "<path> <value>",
+);
+sub cmd_create {
+    my ($self, $opts, $args) = @_;
+    my $path = qualify_path($args->[0] => $self->current_node);
+    $opts->{value} = defined $args->[1] ? $args->[1] : "";
+    $self->handle->create($path, persistent => 1, %$opts);
+    return;
+}
 
-    $self->add_command("delete", code => sub {
-        my ($opts, $args) = @_;
-        my $path = qualify_path($args->[0] => $self->current_node);
-        $self->handle->delete($path);
-        return;
-    }, usage_desc => "<path>");
+add_cmd delete => (
+    usage_desc => "<path>",
+);
+sub cmd_delete {
+    my ($self, $opts, $args) = @_;
+    my $path = qualify_path($args->[0] => $self->current_node);
+    $self->handle->delete($path);
+    return;
+}
 
-    $self->add_command("exit", code => sub { exit 0 });
+add_cmd exit => (code => sub { exit 0 });
 
-    $self->add_command("get", code => sub {
-        my ($opts, $args) = @_;
-        my $path = qualify_path($args->[0] => $self->current_node);
-        return scalar $self->handle->get($path);
-    }, usage_desc => "<path>");
+add_cmd get => (
+    usage_desc => "<path>",
+);
+sub cmd_get {
+    my ($self, $opts, $args) = @_;
+    my $path = qualify_path($args->[0] => $self->current_node);
+    return scalar $self->handle->get($path);
+}
 
-    $self->add_command("get_acl", code => sub {
-        my ($opts, $args) = @_;
-        my $path = qualify_path($args->[0] => $self->current_node);
-        return $self->dump_acl($self->handle->get_acl($path));
-    }, usage_desc => "<path>");
+add_cmd get_acl => (
+    usage_desc => "<path>",
+);
+sub cmd_get_acl {
+    my ($self, $opts, $args) = @_;
+    my $path = qualify_path($args->[0] => $self->current_node);
+    return $self->dump_acl($self->handle->get_acl($path));
+}
 
-    $self->add_command("help", code => sub {
-        my @commands = sort {$a->name cmp $b->name} values %{$self->commands};
-        my $output = "\n";
-        $output .= $_->usage for @commands;
-        return $output;
-    });
+add_cmd help => ();
+sub cmd_help {
+    my ($self) = @_;
+    my @commands = sort {$a->name cmp $b->name} values %{$self->commands};
+    my $output = "\n";
+    $output .= $_->usage for @commands;
+    return $output;
+}
 
-    $self->add_command("ls", code => sub {
-        my ($opts, $args) = @_;
-        my $path = qualify_path($args->[0]//"" => $self->current_node);
-        $path = qualify_path($path => $self->current_node);
-        return join ' ', $self->handle->get_children($path);
-    }, usage_desc => "<path>");
+add_cmd ls => (
+    usage_desc => "<path>",
+);
+sub cmd_ls {
+    my ($self, $opts, $args) = @_;
+    my $path = qualify_path($args->[0]//"" => $self->current_node);
+    $path = qualify_path($path => $self->current_node);
+    return join ' ', $self->handle->get_children($path);
+}
 
-    $self->add_command("set", code => sub {
-        my ($opts, $args) = @_;
-        my $path  = qualify_path($args->[0] => $self->current_node);
-        my $value = $args->[1];
-        $self->handle->set($path, $value, %$opts);
-        return;
-    }, opt_spec => [
+add_cmd set => (
+    opt_spec => [
         [ "version|v=i" ],
-    ], usage_desc => "<path> <value>");
+    ],
+    usage_desc => "<path> <value>",
+);
+sub cmd_set {
+    my ($self, $opts, $args) = @_;
+    my $path  = qualify_path($args->[0] => $self->current_node);
+    my $value = $args->[1];
+    $self->handle->set($path, $value, %$opts);
+    return;
+}
 
-    $self->add_command("set_acl", code => sub {
-        my ($opts, $args) = @_;
-        my $path = qualify_path($args->[0] => $self->current_node);
-        my $append  = delete $opts->{append};
-        my $version = delete $opts->{version};
-        my $acls    = [$self->as_acl($opts)];
-        if ($append) {
-            my $existing = $self->handle->get_acl($path);
-            $acls = [@$acls, @$existing];
-        }
-        $self->handle->set_acl($path => $acls, version => $version);
-        return;
-    }, opt_spec => [
-        [ "append|a"                    ],
-        [ "id|i=s",     {required => 1} ],
-        [ "perms|p=s",  {required => 1} ],
-        [ "scheme|s=s", {required => 1} ],
-        [ "version|v=i"                 ],
-    ], usage_desc => "<path>");
+add_cmd set_acl => (
+    opt_spec => [
+        [ "append|a"    ],
+        [ "id|i=s",     ],
+        [ "perms|p=s",  ],
+        [ "scheme|s=s"  ],
+        [ "version|v=i" ],
+    ],
+    usage_desc => "<path>",
+);
+sub cmd_set_acl {
+    my ($self, $opts, $args) = @_;
+    my $path = qualify_path($args->[0] => $self->current_node);
+    my $append  = delete $opts->{append};
+    my $version = delete $opts->{version};
+    my $acls    = [$self->as_acl($opts)];
+    if ($append) {
+        my $existing = $self->handle->get_acl($path);
+        $acls = [@$acls, @$existing];
+    }
+    $self->handle->set_acl($path => $acls, version => $version);
+    return;
+}
 
-    $self->add_command("stat", code => sub {
-        my ($opts, $args) = @_;
-        my $path = qualify_path($args->[0] => $self->current_node);
-        return $self->dump_hash(($self->handle->get($path))[1]);
-    }, usage_desc => "<path>");
+add_cmd stat => (
+    usage_desc => "<path>",
+);
+sub cmd_stat {
+    my ($self, $opts, $args) = @_;
+    my $path = qualify_path($args->[0] => $self->current_node);
+    return $self->dump_hash(($self->handle->get($path))[1]);
+}
 
-    my @watch_opts = qw(data child data exists all);
-    $self->add_command("watch", code => sub {
-        my ($opts, $args) = @_;
-        if ($opts->{all}) { $opts->{$_}++ for @watch_opts };
-        my $path = qualify_path($args->[0] => $self->current_node);
-
-        my $watch  = sub { print "\n", $self->dump_event($_[0]) };
-        my $handle = $self->handle;
-        $handle->get($path, watcher => $watch) if $opts->{data};
-        $handle->exists($path, watcher => $watch) if $opts->{exists};
-        $handle->get_children($path, watcher => $watch) if $opts->{child};
-        return;
-    }, opt_spec => [
+our @watch_opts = qw(data child data exists all);
+add_cmd watch => (
+    opt_spec => [
         map {
             my $short = substr($_, 0, 1);
             [ "$_|$short" ];
         } @watch_opts
-    ], usage_desc => "<path>");
+    ],
+    usage_desc => "<path>",
+);
+sub cmd_watch {
+    my ($self, $opts, $args) = @_;
+    if ($opts->{all}) { $opts->{$_}++ for @watch_opts };
+    my $path = qualify_path($args->[0] => $self->current_node);
+
+    my $watch  = sub { print "\n", $self->dump_event($_[0]) };
+    my $handle = $self->handle;
+    $handle->get($path, watcher => $watch) if $opts->{data};
+    $handle->exists($path, watcher => $watch) if $opts->{exists};
+    $handle->get_children($path, watcher => $watch) if $opts->{child};
+    return;
+}
+
+
+sub BUILD {}
+after BUILD => sub {
+    my ($self) = @_;
+    $self->_setup_cmds
+
 };
 
 sub as_acl {
